8ce8815ad1fd48cdb6c057a61eb80f757bc168c3
Samarth Bhadane <samarthbhadane23@gmail.com>
2025-03-07 22:09:49 +0530

    Add Circle class to calculate area and perimeter

---
+++ Circle.java
@@ -0,0 +1,22 @@
+import java.lang.Math;
+
+// circle class extends shape class
+public class Circle extends Shape {
+    private double radius;
+
+    // constructor
+    public Circle(double radius) {
+        super("Circle");
+        this.radius = radius;
+    }
+
+    @Override
+    public double calculateArea() {
+        return Math.PI * radius * radius;
+    }
+
+    @Override
+    public double calculatePerimeter() {
+        return 2 * Math.PI * radius;
+    }
+}
